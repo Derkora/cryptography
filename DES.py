c6fb1eba32f537bd1457497b3f03a75424d05560
Steven Figo <derkora.27@gmail.com>
2024-09-29 00:34:00 +0700

    Update DES.py

--- DES.py
+++ DES.py
@@ -112,6 +112,18 @@ s_boxes = [
     ]
 ]
 
+# Expansion Table
+expansion_table = [
+        32, 1, 2, 3, 4, 5,
+        4, 5, 6, 7, 8, 9,
+        8, 9, 10, 11, 12, 13,
+        12, 13, 14, 15, 16, 17,
+        16, 17, 18, 19, 20, 21,
+        20, 21, 22, 23, 24, 25,
+        24, 25, 26, 27, 28, 29,
+        28, 29, 30, 31, 32, 1
+    ]
+
 # Permutation P Table
 p_table = [
     16, 7, 20, 21,
@@ -164,16 +176,6 @@ def permutation_p(sbox_output):
 
 # Expansion function (E) to expand 32 bits to 48 bits
 def bit_expansion(right):
-    expansion_table = [
-        32, 1, 2, 3, 4, 5,
-        4, 5, 6, 7, 8, 9,
-        8, 9, 10, 11, 12, 13,
-        12, 13, 14, 15, 16, 17,
-        16, 17, 18, 19, 20, 21,
-        20, 21, 22, 23, 24, 25,
-        24, 25, 26, 27, 28, 29,
-        28, 29, 30, 31, 32, 1
-    ]
     return ''.join(right[expansion_table[i] - 1] for i in range(48))
 
 # DES Feistel function
